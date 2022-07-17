23493457ca784ca3325b6b80b9eeb2248491688a
Necib ÇAPAR <necipcapar@gmail.com>
2022-07-17 10:46:37 +0300

    Change the name of the pkg. from 'docbook' to 'docbook-xml5'

--- SlackBuild/docbook-xml5/doinst.sh
+++ SlackBuild/docbook-xml5/doinst.sh
@@ -1,6 +1,6 @@
 cd etc/xml/
 cp --archive docbook docbook.docbook5.0.0_orig
 sed_appended_text='\
-  <!-- Added when "docbook-5.0.0" package is installed --> \
+  <!-- Added when "docbook-xml5" package is installed --> \
   <nextCatalog catalog="file:///usr/share/xml/docbook/5.0/catalog_docbook-5.0.0.xml" />'
 sed -i "\$i \ $sed_appended_text" docbook
